2088350032cb0b559566dbb62e86a68af2a40401
landyking <landykingdom@gmail.com>
2017-07-03 17:44:48 +0800

    将每个例子保存为单独的测试类

--- learn-activiti/src/main/java/com/github/landyking/learnActiviti/DeployProcess.java
+++ learn-activiti/src/main/java/com/github/landyking/learnActiviti/DeployProcess.java
@@ -2,11 +2,16 @@ package com.github.landyking.learnActiviti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.apache.log4j.PropertyConfigurator;
 
-public class BasicUseEngine {
-    public static void main(String[] args) {
+import java.text.ParseException;
+
+public class DeployProcess {
+    public static void main(String[] args) throws ParseException {
         PropertyConfigurator.configure(BasicUseEngine.class.getResourceAsStream("/log4j-1.properties"));
         ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
                 .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
@@ -18,5 +23,15 @@ public class BasicUseEngine {
         String pName = processEngine.getName();
         String ver = ProcessEngine.VERSION;
         System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("onboarding.bpmn20.xml").deploy();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId()).singleResult();
+        System.out.println(
+                "Found process definition ["
+                        + processDefinition.getName() + "] with id ["
+                        + processDefinition.getId() + "]");
     }
 }
