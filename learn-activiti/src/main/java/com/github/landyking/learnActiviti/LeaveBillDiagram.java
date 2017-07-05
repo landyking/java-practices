3eb3cd7ad7b5b8bb1b0612b77006898e64061135
landyking <landykingdom@gmail.com>
2017-07-05 16:45:02 +0800

    增加编码定义流程示例代码 增加会签示例代码

---
+++ learn-activiti/src/main/java/com/github/landyking/learnActiviti/LeaveBillDiagram.java
@@ -0,0 +1,47 @@
+package com.github.landyking.learnActiviti;
+
+import org.activiti.bpmn.BpmnAutoLayout;
+import org.activiti.bpmn.model.BpmnModel;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Description：TODO <br/>
+ *
+ * @author: Landy
+ * @date: 2017/7/5 15:04
+ * note:
+ */
+public class LeaveBillDiagram {
+    public static void main(String[] args) throws IOException {
+        PropertyConfigurator.configure(BasicUseEngine.class.getResourceAsStream("/log4j-2.properties"));
+        ProcessEngineConfiguration configuration = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
+                .setJdbcDriver("org.h2.Driver")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setActivityFontName("微软雅黑")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        ProcessEngine processEngine = configuration.buildProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deploy = repositoryService.createDeployment().addClasspathResource("leaveBill.bpmn20.xml").deploy();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().deploymentId(deploy.getId()).singleResult();
+
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinition.getId());
+        new BpmnAutoLayout(bpmnModel).execute();
+        deploy = repositoryService.createDeployment().addBpmnModel("leaveBill.bpmn20.xml", bpmnModel).deploy();
+        processDefinition = repositoryService.createProcessDefinitionQuery().deploymentId(deploy.getId()).singleResult();
+        File destination = new File("target/diagram.png");
+        System.out.println(destination.getAbsolutePath());
+        FileUtils.copyInputStreamToFile(repositoryService.getProcessDiagram(processDefinition.getId()), destination);
+    }
+}
