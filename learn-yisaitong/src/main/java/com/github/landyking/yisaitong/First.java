85bd0214bc502d6c9409435b78c495d8b3155394
landyking <landykingdom@gmail.com>
2017-09-25 17:06:26 +0800

    yisaitong自动脱密

--- learn-yisaitong/src/main/java/com/github/landyking/yisaitong/First.java
+++ learn-yisaitong/src/main/java/com/github/landyking/yisaitong/First.java
@@ -1,13 +1,16 @@
 package com.github.landyking.yisaitong;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.*;
+import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
- * Description：TODO <br/>
+ * 环境：可以正常解密的环境<br/>
+ * 流程：<br/>
+ * 1.复制java.exe为EXCEL.exe（WINWORD.EXE）<br/>
+ * 2.当前目录下打开命令行：./WINWORD.EXE -jar ${this}.jar *.docx 或 ./EXCEL.EXE -jar ${this}.jar *.xls<br/>
  *
  * @author: Landy
  * @date: 2017/8/25 11:09
@@ -15,12 +18,17 @@ import java.io.IOException;
  */
 public class First {
 
-    public static void main(String[] args) throws IOException, InvalidFormatException {
-        Workbook workbook = WorkbookFactory.create(new File("E:\\XDJA\\codes\\workspace\\daydayup\\learn-yisaitong\\src\\main\\resources\\test.xls"));
-        /*Sheet sheet = workbook.getSheetAt(0);
-        Row row = sheet.getRow(0);
-        Cell cell = row.getCell(0);
-        System.out.println(cell.getStringCellValue());*/
-
+    public static void main(String[] args) throws IOException {
+        if (args.length > 0) {
+            String input = args[0];
+            File inputFile = new File(input);
+            System.out.println("input: " + inputFile.getAbsolutePath());
+            String output = UUID.randomUUID().toString().replaceAll("-", "");
+            File outputFile = new File(inputFile.getParent(), output);
+            Files.copy(inputFile, outputFile);
+            System.out.println("output: " + outputFile.getAbsolutePath());
+        } else {
+            System.out.println("need one argument!");
+        }
     }
 }
